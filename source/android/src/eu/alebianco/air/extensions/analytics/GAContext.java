8099072f96cd7cdace835339a0b2b3203c41ea69
Alessandro Bianco <info@alessandrobianco.eu>
2014-03-03 14:31:35 +0000

    update build version

--- source/android/src/eu/alebianco/air/extensions/analytics/GAContext.java
+++ source/android/src/eu/alebianco/air/extensions/analytics/GAContext.java
@@ -18,7 +18,7 @@ import java.util.Map;
 
 public class GAContext extends FREContext {
 
-    public static final String VERSION = "0.2.05";
+    public static final String VERSION = "2.0.6";
     public static final String PRODUCT = "ANE-Google-Analytics";
 
     @Override
