4fc12631c7b977803e56d8f2994780b81604f201
Alessandro Bianco <info@alessandrobianco.eu>
2014-06-06 11:35:32 +0100

    Added basic Xcode project for iOS library

---
+++ native/ios/NativeGATracker/NativeGATracker.m
@@ -0,0 +1,13 @@
+//
+//  NativeGATracker.m
+//  NativeGATracker
+//
+//  Created by Alessandro Bianco on 06/06/2014.
+//  Copyright (c) 2014 Alessandro Bianco. All rights reserved.
+//
+
+#import "NativeGATracker.h"
+
+@implementation NativeGATracker
+
+@end
